4054aa2b21386e4781f383d56d7b49e8fcbbc22d
Byron Murray <byron@beezkneez.nz>
2024-06-24 21:44:30 +1200

    dd Firebase config to .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJzFa73zt5jcMfOlDmvO503zt-sn_y0aA",
-  authDomain: "blog-bc3b2.firebaseapp.com",
-  databaseURL: "https://blog-bc3b2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "blog-bc3b2",
-  storageBucket: "blog-bc3b2.appspot.com",
-  messagingSenderId: "545078579099",
-  appId: "1:545078579099:web:e3c1db5d2fb2e426541b3e"
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
